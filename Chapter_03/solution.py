7069029122cb6f1b26e7e9445af322859f537f68
Todd Anderson <sleepycloudtodd@gmail.com>
2017-05-09 15:49:26 -0500

    update

--- Chapter_03/solution.py
+++ Chapter_03/solution.py
@@ -1,9 +1,13 @@
 def calculate_temp(num_clicks):
-# TODO calculate the temperature (use the variable name of temp)
+# TODO calculate the temperature (use the variable name of temp) num clicks will be number of clicks 
+# #put answer in the temp variable
+    calculate_temp(num_clicks) = (num_clicks % 50) + 40
+    num_clicks = clicks
+    print(num_clicks)
 
     return temp
 
 if __name__ == '__main__':
     clicks_str = input("By how many clicks has the dial been turned? ")
     clicks = int(clicks_str)
-    print(calculate_temp(clicks))
+    print(calculate_temp(num_clicks))

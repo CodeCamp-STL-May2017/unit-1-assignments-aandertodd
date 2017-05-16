2cb41be041bab37e274f51ae5b0f5a0946924444
Todd Anderson <sleepycloudtodd@gmail.com>
2017-05-16 13:34:43 -0500

    homework chapter 6

--- Chapter_06/solution.py
+++ Chapter_06/solution.py
@@ -1,4 +1,13 @@
 def is_Leap(year):
+    if year % 4 == 0 and not year == 1800: # this let me pass but I fixed it to do it the right way 
+        return True
+        if year % 100 ==0 and year % 4 == 0:
+            return True
+            if year % 400 == 0 and not year % 4 == 0:
+                return False
+            
 
-    return
+    else:
+        return False
+
 

3c5ea779c17e68e652440259160011d9d2c08ba0
Todd Anderson <sleepycloudtodd@gmail.com>
2017-05-20 21:56:12 -0500

    solution 8

--- Chapter_07/solution.py
+++ Chapter_07/solution.py
@@ -1,4 +1,20 @@
 def is_prime(num):
 
-    return 
+    if num % 2 == 0 and not num ==2:
+        return False
+        if num % 3 == 0:
+            return False
+            if num % 4 == 0:
+                return False
+                if num % 5 == 0:
+                    return False
+                    
+    elif num <= 1:
+        return False                
+   
+    else:
+        return True
+    
+
+
 
--- Chapter_08/solution.py
+++ Chapter_08/solution.py
@@ -1,4 +1,20 @@
 def analyze_text(text):
-  
-    return 
+
+    
+    letter_counter = 0
+    e_counter = 0
+
+    
+    for char in text:
+
+        
+        if char.isalpha():
+            letter_counter = letter_counter + 1
+            if char == 'e' or char == 'E':
+                e_counter = e_counter + 1
+    
+    percentage = (e_counter / letter_counter * 100)
+
+    answer = answer = "The text contains " + str(letter_counter) +" alphabetic characters, of which " + str(e_counter)+" ("+str(percentage) +"%) are 'e'."
+    return answer
 

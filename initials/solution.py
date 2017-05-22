4373628f7cc6d35d4c25aa8178c622c3f1d8d358
Todd Anderson <sleepycloudtodd@gmail.com>
2017-05-22 17:12:58 -0500

    initials

--- initials/solution.py
+++ initials/solution.py
@@ -1,6 +1,11 @@
 def get_initials(fullname):
-    """ Given a person's name, returns the person's initials in uppercase """
-    # TODO your code here
-
-    return 
-
+    #get first init
+    first_init = fullname[0]
+    #find ' '
+    for char in range(len(fullname)):
+        if fullname[char] == ' ':
+#for every instance of ' ', return the character to the right as an upper().
+            next_init = fullname[char + 1]
+            first_init += next_init
+            # while char == next_init:
+    return first_init.upper()

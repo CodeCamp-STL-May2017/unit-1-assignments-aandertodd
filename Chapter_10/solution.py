0d40eeea0354f5a60f4abd647a438a3b7d86b129
Todd Anderson <sleepycloudtodd@gmail.com>
2017-05-24 13:45:18 -0500

    solution10

--- Chapter_10/solution.py
+++ Chapter_10/solution.py
@@ -1,3 +1,15 @@
 def find_average(nums):
-
-    return 
+    nums.remove(max(nums))
+    nums.remove(min(nums))
+    
+    average = []
+    for num in nums:
+        sum = 0
+        for total in nums:
+            sum += total
+            avg = sum/len(nums)
+        average.append(avg)
+         
+        
+    return avg
+         

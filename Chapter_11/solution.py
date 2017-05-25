790e48f85eb6903d55b2f51965d45e7b6aeccb60
Todd Anderson <sleepycloudtodd@gmail.com>
2017-05-25 11:17:10 -0500

    'solution' 11'

--- Chapter_11/solution.py
+++ Chapter_11/solution.py
@@ -1,3 +1,16 @@
 def get_country_codes(nums):
+
     
-    return	
+    codes = nums.replace('$','').replace('0','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','')
+    return_code = ''.join([code for code in codes if not codes.isdigit()])
+    
+    
+        
+        
+            
+    
+    
+ 
+    
+    
+    return return_code

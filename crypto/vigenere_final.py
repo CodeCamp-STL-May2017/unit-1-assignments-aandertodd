ff5a834ba6a4431e7efc3c8197e91e80b97fc64b
Todd Anderson <sleepycloudtodd@gmail.com>
2017-08-16 20:41:48 -0500

    Add files via upload

--- crypto/vigenere_final.py
+++ crypto/vigenere_final.py
@@ -3,21 +3,32 @@ from .helpers import alphabet_position, rotate_character
 
 def encrypt(message, key):
 
+    # initialize empty message
     encrypted_message = ''
+
+    # initialize key iterator
+    key_iterator = 0
     total_non_alph = 0
+
+    # encrypt letters, let non-alpha characters pass
     for i in range(len(message)):
 
         if message[i].isalpha():
-            
-            key_letter = key[total_non_alph % len(key)] #this is an equation to find key index
-            
+
+            # use the non_alph iterator to find the new letter index
+            key_letter = key[key_iterator % len(key)]
+
             key_index = alphabet_position(key_letter)
             encrypted_letter = rotate_character(message[i], key_index)
-            
+
             encrypted_message += encrypted_letter
             total_non_alph += 1
+            key_iterator += 1
+        # all non-letters will go here
         else:
             encrypted_message += message[i]
 
+
+    return encrypted_message
 
     return encrypted_message
